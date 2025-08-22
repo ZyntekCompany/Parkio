df7a7a6b7ffe6d4bc656256097655e4b2da04ee5
Santiago Suecun <nspes2020@gmail.com>
2025-08-22 00:23:34 -0500

    wrapper header logo error

--- components/auth/wrapper-header.tsx
+++ components/auth/wrapper-header.tsx
@@ -9,9 +9,7 @@ export function WrapperHeader({ title, subtitle }: WrapperHeaderProps) {
   return (
     <div className="flex flex-col items-center gap-2 pt-0">
       <Logo
-        name="Parkio"
-        dinamicTextColor
-        className="md-plus:hidden mb-6 flex flex-col"
+        dark={false}
       />
       <h3 className="max-md-plus:text-[26px] text-3xl leading-none text-center">
         {title}
